dbaca19e763797d826d1da74391ceada347e66ff
blackcodex25 <yuttanakodhod@gmail.com>
2024-07-11 09:50:34 +0700

    Finish Class Conditions if else, else if, else, nested if

--- Conditions/if_else_Statement/if_else_error/if_else_err.go
+++ Conditions/if_else_Statement/if_else_error/if_else_err.go
@@ -1,6 +1,16 @@
 package main
-
+import(f "fmt")
 // รูปแบบที่ไม่ถูกต้อง if else
+// *หากเราใช้คำสั่ง else ในบรรทัดใหม่ จะเกิดข้อผิดพลาด
 func main() {
-
+	temperature := 14
+	if temperature > 15{
+		f.Println("ข้างนอกมันอุ่น")
+	} // ใช้คำสั่ง else ในบรรทัดใหม่ จะทำให้เกิด error 
+	else {
+		f.Println("ข้างในมันเย็น")
+	}
 }
+/*สรุป
+ในภาษา Go 
+*/
